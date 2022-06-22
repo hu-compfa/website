1531346af3b3d572a3e43b1b3bd62919108eed19
Abdullah <gamersspeaks@gmail.com>
2022-06-22 23:10:23 +0300

    chore: add static_url

--- website_for_collage/wsgi_windows.py
+++ website_for_collage/wsgi_windows.py
@@ -2,9 +2,11 @@ import os
 import sys
 import site
 
+
 # Add the app's directory to the PYTHONPATH
-sys.path.append('C:/Users/Administrator/Documents/website_for_collage')
-sys.path.append('C:/Users/Administrator/Documents/website_for_collage/website_for_collage')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.dirname(__file__))
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'website_for_collage.settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website_for_collage.settings")
